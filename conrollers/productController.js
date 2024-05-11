b459865127909fbe1e458c4fb53ca8c6bd5ca7aa
Saim <admin@gmail.com>
2024-05-11 22:57:45 +0600

    afdsfsd

--- conrollers/productController.js
+++ conrollers/productController.js
@@ -21,7 +21,7 @@ const getProducts = async (req, res) => {
             .json({ message: "success", data: results });
     } catch (err) {
         res.status(500)
-            .json({ message: "Internal server error" });
+            .json({ message: "Internal server error",type:err.message });
     }
 }
 
